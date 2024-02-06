0ed415e9b03b9c7744544ac4e37ad4b9638aecfc
Aida <aida2001@mail.ru>
2024-02-06 14:44:51 +0600

    api weather using axios

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,67 @@
 <template>
     <div class="wrapper">
-        <h1>Погодное приложение</h1>
-        <p>Узнать прогноз погоды</p>
-        <input type="text" placeholder="введите город" >
-        <button>Получить погоду</button>
+        <h1>Погодное приложение </h1>
+        <p>Узнать прогноз погоды в {{ city === '' ? 'вашем городе' : cityName }}</p>
+        <input type="text" v-model="city" placeholder="введите город" >
+        <button v-show="city" @click="getWeather()">Получить погоду</button>
+        <p class="error"> {{}}</p>
     </div>
+
+    <div v-if="info != null">
+        <p>{{ showTemp }}</p>
+        <p>{{ showFeelsLike }}</p>
+        <p>{{ showMinTemp }}</p>
+        <p>{{ showMaxTemp }}</p>
+    </div>
+
 </template>
 
 <script>
+import axios from 'axios'
+export default {
 
+    //храним в data() все переменные с которыми взаимодействуем в этом компоненте
+    data() {
+        return {
+            city: '',
+            error: '',
+            info: null
+        }
+    },
+    //computed cвойство - это автоматическое обрабатываемое сво-во
+    computed: {
+        cityName() {
+            return "*" + this.city + "*"
+        },
+        showTemp() {
+            return "Температура: " + this.info.main.temp
+        },
+        showFeelsLike() {
+            return "Ощущается как: " + this.info.main.feels_like
+        },
+        showMinTemp() {
+            return "Минимальная температура: " + this.info.main.temp_min
+        },
+        showMaxTemp() {
+            return "Максимальная температура: " + this.info.main.temp_max
+        },
+
+    },
+    //здесь пишутся методы в methods
+    methods: {
+        getWeather() {
+            //trim() удаляет личшние пробелы до и после
+            if(this.city.trim().length < 2) {
+                this.error = 'Нужно название более 1 символа';
+                return false;
+            }
+
+            axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${this.city}&units=metric&appid=3d9de74844d28377e81415151cbe6a66`)
+                .then(res => (this.info = res.data))
+
+        }
+    }
+}
 
 </script>
 
